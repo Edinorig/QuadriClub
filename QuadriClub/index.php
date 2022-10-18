fa50e17bebdc1617cebd0ab43dcd0d5563eca245
Edinorig <super-ddred@ukr.net>
2022-10-18 13:55:51 +0200

    0.1

---
+++ QuadriClub/index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/import.css">
+    <title>Quadri Club</title>
+</head>
+
+<body>
+
+
+    <header>
+        <div id="background_img1">
+
+            <div class="nav_bar_block">
+                <div class="nav_bar">
+                    <div class="nav_bar_block_logo">
+                        <p class="nav_bar_logo">QuadriClub</p>
+                    </div>
+                    <div class="nav_bar_block_menu">
+                        <div class="nav_bar_menu">
+                            <p class="nav_bar_info">ContactUs</p>
+                        </div>
+                        <div class="nav_bar_menu">
+                            <p class="nav_bar_info"><a href="./php/login.php">LogIn</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="background_main_block">
+                <p class="header_first_inf">QuadriClub</p>
+                <br>
+                <p class="header_second_inf">Best Paintings For Best Friends</p>
+            </div>
+        </div>
+    </header>
+
+    <div class="global_block">
+
+
+        <div class="main_block">
+
+            <div class="main_block_card">
+                <div class="main_block_card_logo">
+                    <p class="card_logo"> Kebab on the wheel</p>
+                </div>
+                <div class="main_block_card_body">
+                    <div class="main_block_card_body_painting">
+                        <img src="./img/quadri/vincent_van_gogh_wheat_field_with_cypresses_wheat_fields_120692_1920x1080.jpg" alt="">
+                    </div>
+                    <div class="main_block_card_body_info">
+                        <div class="main_block_card_body_info_description">
+                            <p class="card_description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+                        </div>
+                        <div class="main_block_card_body_info_author">
+                            <p class="card_author">Davinci_Italia</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="main_block">
+
+            <div class="main_block_card">
+                <div class="main_block_card_logo">
+                    <p class="card_logo"> Kebab on the wheel</p>
+                </div>
+                <div class="main_block_card_body">
+                    <div class="main_block_card_body_painting">
+                        <img src="./img/quadri/MainNotteFHD.jpg" alt="">
+                    </div>
+                    <div class="main_block_card_body_info">
+                        <div class="main_block_card_body_info_description">
+                            <p class="card_description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+                        </div>
+                        <div class="main_block_card_body_info_author">
+                            <p class="card_author">Davinci_Italia</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="main_block">
+
+            <div class="main_block_card">
+                <div class="main_block_card_logo">
+                    <p class="card_logo"> Kebab on the wheel</p>
+                </div>
+                <div class="main_block_card_body">
+                    <div class="main_block_card_body_painting">
+                        <img src="./img/quadri/vidomi-kartyni11.jpg" alt="">
+                    </div>
+                    <div class="main_block_card_body_info">
+                        <div class="main_block_card_body_info_description">
+                            <p class="card_description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+                        </div>
+                        <div class="main_block_card_body_info_author">
+                            <p class="card_author">Davinci_Italia</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+
+    <footer>
+        <div class="footer_block">
+            <p class="footer_info">Email: infoQuadr.gmail.com</p>
+        </div>
+        <div class="footer_block">
+            <p class="footer_info">Telefono: +39 123 324 4759</p>
+        </div>
+    </footer>
+
+</body>
+
+</html>
---
+++ QuadriClub/php/login.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/import.css">
+    <title>Login</title>
+</head>
+
+<body>
+
+    <div id="background_img2">
+
+        <div class="nav_bar_block">
+            <div class="nav_bar">
+                <div class="nav_bar_block_logo">
+                    <p class="nav_bar_logo">QuadriClub</p>
+                </div>
+                <div class="nav_bar_block_menu">
+                    <div class="nav_bar_menu">
+                        <p class="nav_bar_info"><a href="../index.php">Exit</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="form_block">
+
+            <form action="" class="form_login">
+                <input type="text" placeholder="Input login" name="login">
+                <input type="password" placeholder="Input pass" name="password">
+                <button class="btn_confirm" type="submit"><p class="btn_text">Submit</p></button>
+            </form>
+
+        </div>
+
+    </div>
+
+</body>
+
+</html>
