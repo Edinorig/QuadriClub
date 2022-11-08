681aab825765f22a3dea74dbd875825ce46d1db8
Edinorig <super-ddred@ukr.net>
2022-11-08 08:40:14 +0100

    test

---
+++ src/common/php/footer.php
@@ -0,0 +1,8 @@
+<div class="wrapper-footer">
+        <footer>
+
+            <p class="footer-info">Email: infoQuadr.gmail.com</p>
+            <p class="footer-info">Telefono: +39 123 324 4759</p>
+
+        </footer>
+    </div>
