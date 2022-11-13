a81c703647cf893bcd22931d996f9f946362b8f3
Edinorig <super-ddred@ukr.net>
2022-11-13 22:41:52 +0100

    updaye

--- src/common/php/dbConnector.php
+++ src/common/php/dbConnector.php
@@ -1,13 +1,13 @@
 <?php
 session_start();
 
-$_SESSION['address'] = "127.0.0.1";
+$_SESSION['address'] = "127.0.0.1:8889";
 
 
 $_SESSION['user'] = "root";
 
 
-$_SESSION['pwd'] = "";
+$_SESSION['pwd'] = "root";
 
 
 $_SESSION['db'] = "galleria5e";
